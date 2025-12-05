4b82591c9739bfd0e3cce7bf92984b4c96d4b7bc
soypepediaz <100941124+soypepediaz@users.noreply.github.com>
2025-12-05 12:36:49 +0100

    Create __init__.py

---
+++ client_code/Form1/__init__.py
@@ -0,0 +1,8 @@
+from ._anvil_designer import Form1Template
+from anvil import *
+import anvil.server
+
+class Form1(Form1Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
